13426d9c5cc7e47820ea00cfafb60ed9d17cbfda
Ian Mungai <iansam74@gmail.com>
2022-07-04 12:43:35 +0300

    fix: transfer solana from escrow

--- client/src/components/TransferSolana/TransferSolana.js
+++ client/src/components/TransferSolana/TransferSolana.js
@@ -1,4 +1,4 @@
-import { clusterApiUrl, Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from '@solana/web3.js';
+import { clusterApiUrl, Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction, LAMPORTS_PER_SOL } from '@solana/web3.js';
 import { useCallback } from 'react';
 import { useMoralis } from 'react-moralis';
 
@@ -6,18 +6,18 @@ export const SendOneLamportFromEscrowAddress = () => {
     const  { user } = useMoralis();
 
     const onClick = useCallback(async () => {
-        const connection = new Connection(clusterApiUrl('devnet'));
+
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
         const publicKey = new PublicKey(user.get("solAddress"));
 
-
         const secret = Uint8Array.from([85,105,9,5,143,6,151,16,145,226,251,129,35,195,180,119,115,128,163,138,123,125,47,105,176,149,182,84,203,162,169,223,97,231,10,129,35,97,6,149,220,235,224,147,178,234,48,238,172,81,76,170,95,237,199,20,64,30,116,47,24,97,77,72])
-        let escrowKeyPair = Keypair.fromSecretKey(secret);
+        const escrowKeyPair = Keypair.fromSecretKey(secret);
 
         const transaction = new Transaction().add(
             SystemProgram.transfer({
-                fromPubkey: publicKey,
-                toPubkey: escrowKeyPair.publicKey,
-                lamports: 1,
+                fromPubkey: escrowKeyPair.publicKey,
+                toPubkey: publicKey,
+                lamports: LAMPORTS_PER_SOL * 0.5,
             })
         );
 
@@ -25,12 +25,18 @@ export const SendOneLamportFromEscrowAddress = () => {
             connection,
             transaction,
             [escrowKeyPair]
-          );
+        )
+        .then(response => {
+            console.log("response", response);
+        })
+        .catch(error => {
+            console.log("error", error);
+        })
     }, [user]);
 
     return (
         <button onClick={onClick}>
-            Send 1 SOL
+            Send 0.5 SOL
         </button>
     );
 };
