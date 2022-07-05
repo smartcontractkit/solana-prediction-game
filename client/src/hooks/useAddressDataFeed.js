20c2b088f1dcb581074f96ea50629019eebbe42c
Ian Mungai <iansam74@gmail.com>
2022-07-06 02:58:32 +0300

    fix: add initial getDataFeeds before interval

--- client/src/hooks/useAddressDataFeed.js
+++ client/src/hooks/useAddressDataFeed.js
@@ -56,6 +56,7 @@ export function useAddressDataFeed() {
     }
 
     useEffect(() => {
+        getDataFeeds();
         window.interval3min = setInterval(
             () => getDataFeeds(),
             180000 // 3 minutes
