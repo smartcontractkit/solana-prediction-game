c0783c666b89490acb3cf9c80e3b8300b0879e1e
Ian Mungai <iansam74@gmail.com>
2022-07-05 15:19:30 +0300

    fix: refractor files transactions, datafeed, predictions

---
+++ server/predictions/index.js
@@ -0,0 +1,59 @@
+const Moralis = require("moralis/node");
+const dataFeed = require("../dataFeed");
+
+const createPrediction = async (prediction) => {
+
+  const Prediction = Moralis.Object.extend("Prediction");
+  const predictionObject = new Prediction();
+
+  return new Promise(async (resolve, reject) => {
+    await predictionObject.save(prediction)
+    .then(
+      (data) => {
+        // Execute any logic that should take place after the object is saved.
+        console.log("New object created with objectId: " + data.id);
+        resolve(data);
+      },
+      (error) => {
+        // Execute any logic that should take place if the save fails.
+        // error is a Moralis.Error with an error code and message.
+        console.log("Failed to create new object, with error code: " + error.message);
+      }
+    );
+  });
+}
+
+const addPredictionsDaily = async (address, pair) => {
+  const latestRound = await dataFeed.getLatestDataRound(address, pair);
+
+  const { answerToNumber, feed, observationsTS } = latestRound;
+
+  var date = new Date();
+
+  const predictionData = {
+    owner: process.env.OWNER_PUBLIC_ADDRESS,
+    account: feed,
+    pair,
+    prediction: null,
+    expiryTime: null,
+    predictionDeadline: null,
+    openingPredictionPrice: answerToNumber,
+    openingPredictionTime: observationsTS,
+    status: true,
+  };
+
+  const plusOnePercent = await createPrediction({
+    ...predictionData,
+    prediction: latestRound.answerToNumber * 1.01,
+    expiryTime: new Date(date.setDate(date.getDate() + 1)),
+    predictionDeadline: new Date(date.setDate(date.getHours() + 1)),
+  });
+  const minusOnePercent = await createPrediction({
+    ...predictionData,
+    prediction: latestRound.answerToNumber * 0.99,
+    expiryTime: new Date(date.setDate(date.getDate() + 1)),
+    predictionDeadline: new Date(date.setDate(date.getHours() + 1)),
+  });
+
+  return [plusOnePercent, minusOnePercent];
+}
---
+++ server/transactions/index.js
@@ -0,0 +1,34 @@
+const solanaWeb3 = require("@solana/web3.js");
+const { clusterApiUrl, Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction, LAMPORTS_PER_SOL } = solanaWeb3;
+
+escrowTransferSOL = async (toAddress, amount) => {
+
+  const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+  const toPubkey = new PublicKey(toAddress);
+
+  const secret = Uint8Array.from([85,105,9,5,143,6,151,16,145,226,251,129,35,195,180,119,115,128,163,138,123,125,47,105,176,149,182,84,203,162,169,223,97,231,10,129,35,97,6,149,220,235,224,147,178,234,48,238,172,81,76,170,95,237,199,20,64,30,116,47,24,97,77,72])
+  const escrowKeyPair = Keypair.fromSecretKey(secret);
+
+  const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: escrowKeyPair.publicKey,
+        toPubkey,
+        lamports: LAMPORTS_PER_SOL * amount,
+      })
+  );
+
+  return sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [escrowKeyPair]
+  )
+  .then(response => {
+    console.log("response", response);
+    return {
+      transactionId: response
+    };
+  })
+  .catch(error => {
+    console.error("error", error);  
+  })
+}
