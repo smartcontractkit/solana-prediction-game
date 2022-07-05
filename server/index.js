b3ba8f8e779d3467cc4c2bbd095ff892b2963e05
Ian Mungai <iansam74@gmail.com>
2022-07-06 00:05:55 +0300

    feature: createBet,  retrieveBet, retrieveUserBets

--- server/index.js
+++ server/index.js
@@ -4,7 +4,12 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const cors = require("cors");
+const Moralis = require("moralis/node");
 require('dotenv').config();
+
+const serverUrl = process.env.MORALIS_SERVER_URL;
+const appId = process.env.MORALIS_APP_ID;
+const masterKey = process.env.MORALIS_MASTER_KEY;
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 const dataFeed = require("./dataFeed");
 const predictions = require("./predictions");
 const transactions = require("./transactions");
+const bets = require("./bets");
 
 const PORT = process.env.PORT || 3001;
 
@@ -32,20 +38,23 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+  await Moralis.start({ serverUrl, appId, masterKey });
   console.log(`Server listening on ${PORT}`);
 });
 
 app.get('/getLatestDataRound', async (req, res) => {
   const { address, pair } = req.body;
+  if(!address || !pair) {
+    res.status(400).send('Missing address or pair');
+    return;
+  }
   let latestRound = await dataFeed.getLatestDataRound(address, pair);
   res.send(latestRound);
 });
 
 app.post('/addPrediction', async (req, res) => {
   const prediction  = req.body;
-
-  await Moralis.start({ serverUrl, appId, masterKey });
   
   const predictionData = await predictions.createPrediction({
     ...prediction,
@@ -57,20 +66,52 @@ app.post('/addPrediction', async (req, res) => {
   res.send(predictionData);
 });
 
-/* Moralis init code */
-const serverUrl = process.env.MORALIS_SERVER_URL;
-const appId = process.env.MORALIS_APP_ID;
-const masterKey = process.env.MORALIS_MASTER_KEY;
-
 app.get('/scheduleDailyPredictions', async (req, res) => {
   const { address, pair } = req.body;
-  await Moralis.start({ serverUrl, appId, masterKey });
+  if(!address || !pair) {
+    res.status(400).send('Missing address or pair');
+    return;
+  }
   const predictions = await predictions.addPredictionsDaily(address, pair)
   res.send(predictions);
 });
 
 app.post('/escrowTransferSOL', async (req, res) => {
   const { toAddress, amount } = req.body;
+  if(!toAddress || !amount) {
+    res.status(400).send('Missing toAddress or amount');
+    return;
+  }
   const solTransfer = await transactions.escrowTransferSOL(toAddress, amount)
   res.send(solTransfer);
+});
+
+app.post('/createBet', async (req, res) => {
+  const bet = req.body;
+  if(!bet.user || !bet.status || !bet.predictionId || !bet.amount) {
+    res.status(400).send('Missing valid bet');
+    return;
+  }
+  const betData = await bets.createBet(bet);
+  res.send(betData);
+});
+
+app.get('/getBet/:betId', async (req, res) => {
+  const betId = req.params.betId;
+  if(!betId) {
+    res.status(400).send('Missing betId');
+    return;
+  }
+  await bets.retrieveBet(betId);
+  res.send(betData);
+});
+
+app.get('/getUserBets/:user', async (req, res) => {
+  const user = req.params.user;
+  if(!user) {
+    res.status(400).send('Missing user');
+    return;
+  }
+  const betsData = await bets.retrieveUserBets(user);
+  res.send(betsData);
 });
