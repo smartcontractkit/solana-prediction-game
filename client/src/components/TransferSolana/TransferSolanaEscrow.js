ce57b3c1be30f8191d2d486a7ce1c8793ed53372
Victor Chukwuebuka Umeh <41862157+vyktoremario@users.noreply.github.com>
2022-07-18 09:01:55 +0100

    Added an Axios Instance with headers

--- client/src/components/TransferSolana/TransferSolanaEscrow.js
+++ client/src/components/TransferSolana/TransferSolanaEscrow.js
@@ -1,57 +1,51 @@
 import { Button } from '@chakra-ui/react';
 import { useCallback, useState } from 'react';
 import { useMoralis } from 'react-moralis';
+import axiosInstance from '../../helpers/axiosInstance';
 
 export const SendFromEscrowAddress = () => {
-    const { user } = useMoralis();
-    const [ isSending, setIsSending ] = useState(false);
+  const { user } = useMoralis();
+  const [isSending, setIsSending] = useState(false);
 
 
-    const onClick = useCallback(async () => {
-        setIsSending(true);
+  const onClick = useCallback(async () => {
+    setIsSending(true);
 
-        const toAddress = user.get("solAddress");
-        const amount = 0.5;
+    const toAddress = user.get("solAddress");
+    const amount = 0.5;
 
-        fetch(`${process.env.REACT_APP_SERVER_URL}/escrowTransferSOL`,{
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Methods": "GET, POST",
-              "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
-            },
-            body: JSON.stringify({
-                toAddress,
-                amount,
-            })
-          })
-          .then(res => res.json())
-          .then(res => {
-            setIsSending(false);
-            alert("Transfer successful: " + res?.transactionId);
-          })
-          .catch(err => {
-            setIsSending(false);
-            alert("Error occured: " + err.message);
-          });
+    axiosInstance.post('/escrowTransferSOL',
+      {
+        toAddress,
+        amount
+      }
+    )
+      .then(res => res.data)
+      .then(res => {
+        setIsSending(false);
+        alert("Transfer successful: " + res?.transactionId);
+      })
+      .catch(err => {
+        setIsSending(false);
+        alert("Error occured: " + err.message);
+      });
 
-    }, [user]);
+  }, [user]);
 
-    return (
-        <Button
-            size="sm"
-            rounded="md"
-            color={["white", "white", "white", "white"]}
-            bg={["black", "black", "black", "black"]}
-            _hover={{
-                bg: ["primary.700", "primary.700", "primary.700", "primary.700"]
-            }}
-            isLoading={isSending}
-            loadingText="Sending..."
-            onClick={onClick}
-            >
-            Send 0.5 SOL from escrow
-        </Button>
-    );
+  return (
+    <Button
+      size="sm"
+      rounded="md"
+      color={["white", "white", "white", "white"]}
+      bg={["black", "black", "black", "black"]}
+      _hover={{
+        bg: ["primary.700", "primary.700", "primary.700", "primary.700"]
+      }}
+      isLoading={isSending}
+      loadingText="Sending..."
+      onClick={onClick}
+    >
+      Send 0.5 SOL from escrow
+    </Button>
+  );
 };
