3cffd2ebe0351322fc1de50056d8b86bc4ef070c
Ian Mungai <iansam74@gmail.com>
2022-07-21 04:39:24 +0300

    feature: win rate

--- client/src/components/BetSidebar/MyBets.js
+++ client/src/components/BetSidebar/MyBets.js
@@ -1,11 +1,9 @@
-import { Button, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 import { UserDataContext } from "../../contexts/UserDataProvider";
 import axiosInstance from "../../helpers/axiosInstance";
-import emptyBets from '../../assets/bets/empty-bets.svg'
-import placeholder from "../../assets/logos/placeholder.png";
-import { getCurrenciesFromPairs, roundOff } from "../../helpers/sol_helpers";
-import { DIVISOR } from "../../lib/constants";
+import emptyBets from '../../assets/bets/empty-bets.svg';
+import SingleBetCard from "./SingleBetCard";
 
 const MyBets = () => {
     const [ isFetching, setIsFetching ] = useState(true);
@@ -42,121 +40,8 @@ const MyBets = () => {
         </VStack>
     }
 
-    const withdraw = () => {
-        console.log("Withdrawing");
-        // TODO: Withdraw amount won
-    }
-
-
-    const SingleBetCard = ({ bet }) => {
-        const { parent, amount, status } = bet;
-        const { pair, prediction, expiryTime } = parent;
-        const { firstCurrency, secondCurrency } = getCurrenciesFromPairs(pair);
-        const logoImage = require(`../../assets/logos/${firstCurrency.toLowerCase()}.png`);
-
-        let statusText = "";
-        let statusColor = "";
-
-        switch(status) {
-            case "open":
-                statusText = "ONGOING";
-                statusColor = "orange.300";
-                break;
-            case "won":
-                statusText = "WIN";
-                statusColor = "green.400";
-                break;
-            case "lost":
-                statusText = "LOST";
-                statusColor = "red.500";
-                break;
-            default:
-                statusText = "ONGOING";
-                statusColor = "orange.300";
-        }
-
-        return (
-            <VStack
-                rounded="md"
-                bg="whiteAlpha.100"
-                alignItems="flex-start"
-                p="12px"
-                gap="8px"
-                w="100%"
-            >
-                <HStack
-                    borderRadius="6px"
-                    alignItems="center"
-                    justify="space-between"
-                    w="100%"
-                >
-                    <HStack>
-                        <Image
-                            borderRadius='full'
-                            boxSize='24px'
-                            src={logoImage}
-                            fallbackSrc={placeholder}
-                            alt={pair}
-                        />
-                        <Text fontWeight={600} fontSize="12px" color="gray.200">
-                            {pair}
-                        </Text>
-                    </HStack>
-                    <Text fontWeight={600} fontSize="12px" color={statusColor}>
-                        {statusText}
-                    </Text>
-                </HStack>
-                <Text textAlign="left">
-                    {firstCurrency} will settle at {roundOff((prediction / DIVISOR), 5)} {secondCurrency} at {new Date(expiryTime).toLocaleString()}
-                </Text>
-                <HStack textAlign="left">
-                    <Text fontWeight={500} fontSize="xs" color="gray.500">
-                        Bank:
-                    </Text>
-                    <Text fontWeight={700} fontSize="xs" color="purple.200">
-                        { amount * 2 } SOL
-                    </Text>
-                </HStack>
-                <HStack
-                    w="100%"
-                >
-                    <Button
-                        w="41px"
-                        h="24px"
-                        fontSize="12px"
-                        minW="min-content"
-                        variant="outline"
-                        py="4px"
-                        borderRadius="6px"
-                        flexGrow={1}
-                    >   
-                        Details
-                    </Button>
-                    {
-                        status === "won" && (
-                            <Button
-                                w="41px"
-                                h="24px"
-                                fontSize="12px"
-                                minW="min-content"
-                                variant="outline"
-                                px="4px"
-                                borderRadius="6px"
-                                flexGrow={1}
-                                color="gray.800"
-                                bg="green.200"
-                                onClick={withdraw}
-                            >
-                                Withdraw
-                            </Button>
-                        )
-                    }
-                </HStack>
-
-            </VStack>
-        )
-    }
-
+    const betsWon = bets.filter(bet => bet.status === "won").length;
+    const winRate = betsWon.length / bets.length;
 
     return (
         <VStack 
@@ -171,7 +56,7 @@ const MyBets = () => {
                     {bets.length} bets
                 </Text>
                 <Text color="gray.500">
-                    Win Rate: 45%
+                    Win Rate: {winRate} %
                 </Text>
             </HStack>
             <VStack
---
+++ client/src/components/BetSidebar/SingleBetCard.js
@@ -0,0 +1,121 @@
+import { Button, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import placeholder from "../../assets/logos/placeholder.png";
+import { getCurrenciesFromPairs, roundOff } from "../../helpers/sol_helpers";
+import { DIVISOR } from "../../lib/constants";
+
+const SingleBetCard = ({ bet }) => {
+    const { parent, amount, status } = bet;
+    const { pair, prediction, expiryTime } = parent;
+    const { firstCurrency, secondCurrency } = getCurrenciesFromPairs(pair);
+    const logoImage = require(`../../assets/logos/${firstCurrency.toLowerCase()}.png`);
+
+    let statusText = "";
+    let statusColor = "";
+
+    switch(status) {
+        case "open":
+            statusText = "ONGOING";
+            statusColor = "orange.300";
+            break;
+        case "won":
+            statusText = "WIN";
+            statusColor = "green.400";
+            break;
+        case "lost":
+            statusText = "LOST";
+            statusColor = "red.500";
+            break;
+        default:
+            statusText = "ONGOING";
+            statusColor = "orange.300";
+    }
+
+
+    const withdraw = () => {
+        console.log("Withdrawing");
+        // TODO: Withdraw amount won
+    }
+
+    return (
+        <VStack
+            rounded="md"
+            bg="whiteAlpha.100"
+            alignItems="flex-start"
+            p="12px"
+            gap="8px"
+            w="100%"
+        >
+            <HStack
+                borderRadius="6px"
+                alignItems="center"
+                justify="space-between"
+                w="100%"
+            >
+                <HStack>
+                    <Image
+                        borderRadius='full'
+                        boxSize='24px'
+                        src={logoImage}
+                        fallbackSrc={placeholder}
+                        alt={pair}
+                    />
+                    <Text fontWeight={600} fontSize="12px" color="gray.200">
+                        {pair}
+                    </Text>
+                </HStack>
+                <Text fontWeight={600} fontSize="12px" color={statusColor}>
+                    {statusText}
+                </Text>
+            </HStack>
+            <Text textAlign="left">
+                {firstCurrency} will settle at {roundOff((prediction / DIVISOR), 5)} {secondCurrency} at {new Date(expiryTime).toLocaleString()}
+            </Text>
+            <HStack textAlign="left">
+                <Text fontWeight={500} fontSize="xs" color="gray.500">
+                    Bank:
+                </Text>
+                <Text fontWeight={700} fontSize="xs" color="purple.200">
+                    { amount * 2 } SOL
+                </Text>
+            </HStack>
+            <HStack
+                w="100%"
+            >
+                <Button
+                    w="41px"
+                    h="24px"
+                    fontSize="12px"
+                    minW="min-content"
+                    variant="outline"
+                    py="4px"
+                    borderRadius="6px"
+                    flexGrow={1}
+                >   
+                    Details
+                </Button>
+                {
+                    status === "won" && (
+                        <Button
+                            w="41px"
+                            h="24px"
+                            fontSize="12px"
+                            minW="min-content"
+                            variant="outline"
+                            px="4px"
+                            borderRadius="6px"
+                            flexGrow={1}
+                            color="gray.800"
+                            bg="green.200"
+                            onClick={withdraw}
+                        >
+                            Withdraw
+                        </Button>
+                    )
+                }
+            </HStack>
+
+        </VStack>
+    )
+}
+
+export default SingleBetCard;
