125d634c786a11c7aa74dd120456fdb9d7db75d6
Ian Mungai <iansam74@gmail.com>
2022-08-01 15:56:52 +0300

    chore: api/bet/status creation

---
+++ api/bets/status.js
@@ -0,0 +1,32 @@
+const { connectToDatabase } = require("../../lib/mongoose");
+const Bet = require("../../models/bet.model");
+const Prediction = require("../../models/prediction.model");
+
+module.exports = async (req, res) => {
+
+    if (req.method === 'POST') {
+        try {
+            await connectToDatabase();
+
+            const { _id, status } = req.body;
+            if (!_id || !status) {
+                throw new Error("Missing required parameters");
+            }
+            
+            const bet = await Bet.findById(_id);
+            bet.status = status;
+            const result = await bet.save();
+            console.log(`Bet was inserted with the _id: ${result._id}`);
+    
+            res.send(result);
+        } catch (err) {
+            console.error("Failed to update bet, with error code: " + err.message);
+    
+            res.status(500).send(err);
+        } 
+    } else {
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method Not Allowed');
+    }
+
+}
