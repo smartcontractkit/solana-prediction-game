5d0cd7f9da9065f05c29617d406ab1216c972184
Ian Sam Mungai <ian.mungai@thisdot.co>
2022-08-08 19:21:29 +0300

    Update NextPredictionTimer name

--- src/components/Subheader/index.js
+++ src/components/Subheader/index.js
@@ -4,7 +4,7 @@ import CountdownTimer from "./CountdownTimer";
 import chainlinkLogo from "../../assets/logos/chainlink.svg";
 import { useEffect, useState } from "react";
 
-const NextPrediction = () => {
+const NextPredictionTimer = () => {
     const date = new Date();
     const [predictionTime, setPredictionTime] = useState(date.getTime() + (60000 * (10 - (date.getMinutes() % 10))));
 
@@ -91,7 +91,7 @@ const Subheader = () => {
             justifyContent="space-between"
             width="100%"
         >
-            <NextPrediction />
+            <NextPredictionTimer />
             <Hide below="md">
                 <ChainlinkFeedButton />
             </Hide>
