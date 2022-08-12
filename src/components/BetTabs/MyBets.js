3122d0ab80ebbfdcaf6c58347334a986156e99ae
Ian Sam Mungai <ian.mungai@thisdot.co>
2022-08-12 14:46:37 +0300

    Loading My bets (#77)
    * loading my bets
    
    * fix: mybets null
    
    * chore: not logged in mybets comp
    
    * fix: typo connect wallet

--- src/components/BetTabs/MyBets.js
+++ src/components/BetTabs/MyBets.js
@@ -1,34 +1,71 @@
-import { HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { HStack, Image, Skeleton, SkeletonCircle, SkeletonText, Text, VStack } from "@chakra-ui/react";
 import { useContext } from "react";
 import { UserDataContext } from "../../contexts/UserDataProvider";
 import emptyBets from '../../assets/icons/empty-bets.svg';
 import SingleBetCard from "./SingleBetCard";
 import { roundOff } from "../../helpers/solHelpers";
+import WalletModalButton from "../WalletModalButton/WalletModalButton";
 
 const MyBets = () => {
 
-    const { myBets } = useContext(UserDataContext);
-    
-    const EmptyBets = () => (
-        <VStack>
-            <Image src={emptyBets} height="64px" alt="empty bet slip" my="10px" />
-            <Text fontWeight={700} color="gray.200">
-                No bets here yet
-            </Text>
-            <Text color="gray.500">
-                Make your first one and it will appear here.
-            </Text>
-        </VStack>
-    )
+    const { myBets, user } = useContext(UserDataContext);
 
+    if(!user){
+        return (
+            <VStack>
+                <Image src={emptyBets} height="64px" alt="empty bet slip" my="10px" />
+                <Text fontWeight={700} color="gray.200">
+                    No bets here yet
+                </Text>
+                <Text color="gray.500">
+                    Connect your wallet to see your bets.
+                </Text>
+                <WalletModalButton 
+                    width="100%"
+                    rounded="md"
+                    color="gray.800"
+                    bg="blue.200"
+                    _hover={{
+                        bg: "blue.100",
+                    }}
+                />
+            </VStack>
+        )
+    }
+    
     if(!myBets) {
-        return <EmptyBets />
+        return (
+            <VStack
+                rounded="md"
+                bg="whiteAlpha.100"
+                alignItems="flex-start"
+                p="12px"
+                gap="8px"
+                w="100%"
+                boxShadow='lg' 
+            >
+                <SkeletonCircle boxSize='24px' opacity={0.3} />
+                <SkeletonText noOfLines={1} spacing='4' opacity={0.3} w='100%' />
+                <SkeletonText noOfLines={1} spacing='4' opacity={0.3} w='70%' />
+                <SkeletonText noOfLines={1} spacing='4' opacity={0.3} w='100%' />
+                <Skeleton height='32px' rounded="sm" opacity={0.3} w='100%' />
+            </VStack>
+        )
     }
 
     if(myBets.length === 0) {
-        return <EmptyBets />
+        return (
+            <VStack>
+                <Image src={emptyBets} height="64px" alt="empty bet slip" my="10px" />
+                <Text fontWeight={700} color="gray.200">
+                    No bets here yet
+                </Text>
+                <Text color="gray.500">
+                    Make a bet and it will appear here
+                </Text>
+            </VStack>
+        )
     }
-
 
     const myBetsWon = myBets.filter(bet => (bet.status === "won" || bet.status === "completed"));
     const winRate = ((myBetsWon.length || myBets.length) === 0) ? 0 : roundOff(myBetsWon.length / myBets.length, 2) * 100;
