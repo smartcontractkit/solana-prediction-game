1c76b7ed306c721fb26bc50180fac3733ccd77e0
Ian Mungai <iansam74@gmail.com>
2022-08-16 13:47:44 +0300

    edit bets api

--- api/bets/add.js
+++ api/bets/add.js
@@ -4,8 +4,14 @@ const Bet = require("../../models/bet.model");
 const Prediction = require("../../models/prediction.model");
 
 /**
- * Vercel cloud function for the creation of a new bet
-*/
+ * This function is deployed as a standalone endpoint via Vercel Cloud Functions. Given the expected 
+ * request payload, it generates a new bet entity and stores it in MongoDB. The request
+ * is expected to come in as a POST request to `/api/bets/add`. The request body should have the shape:
+ * { user: string, prediction: string, amount: number, transactionSignature: string, status: "completed" | "won" | "lost" | "ongoing" }
+ *
+ * @param req NextApiRequest HTTP request object wrapped by Vercel function helpers
+ * @param res NextApiResponse HTTP response object wrapped by Vercel function helpers
+ */
 module.exports = async (req, res) => {
 
     if (req.method === 'POST') {
@@ -14,11 +20,13 @@ module.exports = async (req, res) => {
             
             const bet = req.body;
 
-            // Cast the prediction and user id to a mongoose ObjectId
+            // Here we cast the entity IDs for prediction and user to be MongoDB valid ObjectIds. 
+            // MongoDB requires all IDs be in this format.
             bet.prediction = Types.ObjectId(bet.prediction);
             bet.user = Types.ObjectId(bet.user);
 
-            // Create the bet
+            // This instantiates the bet with our modified payload and stores the object 
+            // into our MongoDB database using the mongoose driver.
             const betObject = new Bet(bet);
             const result = await betObject.save();
             console.log(`Bet was inserted with the _id: ${result._id}`);
--- api/bets/index.js
+++ api/bets/index.js
@@ -1,10 +1,15 @@
 const { connectToDatabase } = require("../../lib/mongoose");
 const Bet = require("../../models/bet.model");
 const Prediction = require("../../models/prediction.model");
-
 /**
- * Vercel cloud function for getting bet data based on query params
-*/
+ * This function is deployed as a standalone endpoint via Vercel Cloud Functions. Given the expected 
+ * request query payload, it retrieves bet entities from MongoDB based on queries from the Mongoose driver. 
+ * The request is expected to come in as a GET request to `/api/bets`. The request query can be empty or follow
+ * the model data  at /models/bet.model.js and mongoose docs: https://mongoosejs.com/docs/queries.html.
+ *
+ * @param req NextApiRequest HTTP request object wrapped by Vercel function helpers
+ * @param res NextApiResponse HTTP response object wrapped by Vercel function helpers
+ */
 module.exports = async (req, res) => {
 
   try {
