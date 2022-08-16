8ed0a8a714833e01ce64bfc59f009ceea374d3dd
Ian Mungai <iansam74@gmail.com>
2022-08-16 13:47:44 +0300

    edit bets api

--- api/bets/getBet.js
+++ api/bets/getBet.js
@@ -1,10 +1,15 @@
 const { connectToDatabase } = require("../../lib/mongoose");
 const Bet = require("../../models/bet.model");
 const Prediction = require("../../models/prediction.model");
-
 /**
- * Vercel cloud function for the getting single bet data based on query params
-*/
+ * This function is deployed as a standalone endpoint via Vercel Cloud Functions. Given the expected 
+ * request query payload, it retrieves a single bet entity from MongoDB based on queries from the Mongoose driver. 
+ * The request is expected to come in as a GET request to `/api/bets/getBet`. The request query can be empty or follow
+ * the model data  at /models/bet.model.js and mongoose docs: https://mongoosejs.com/docs/queries.html.
+ *
+ * @param req NextApiRequest HTTP request object wrapped by Vercel function helpers
+ * @param res NextApiResponse HTTP response object wrapped by Vercel function helpers
+ */
 module.exports = async (req, res) => {
 
     try {
@@ -12,7 +17,8 @@ module.exports = async (req, res) => {
             
         const searchQuery  = req.query;
 
-        // Get the bet with prediction data
+        // This retrieves a single bet entity with a its parent prediction entity 
+        // via MongoDB database using the mongoose driver.
         const bet = await Bet.findOne(searchQuery)
         .populate("prediction");
 
