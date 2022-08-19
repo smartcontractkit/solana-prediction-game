9573ed9cff504bad37106af5c6d9884be5b8de34
Ian Sam Mungai <ian.mungai@thisdot.co>
2022-08-19 16:39:00 +0300

    Leaderboard (#91)

--- api/bets/add.js
+++ api/bets/add.js
@@ -2,6 +2,7 @@ const { connectToDatabase } = require("../../lib/mongoose");
 const { Types } = require("mongoose");
 const Bet = require("../../models/bet.model");
 const Prediction = require("../../models/prediction.model");
+const User = require("../../models/user.model");
 
 module.exports = async (req, res) => {
 
@@ -14,8 +15,19 @@ module.exports = async (req, res) => {
             bet.user = Types.ObjectId(bet.user);
             const betObject = new Bet(bet);
             
-            const result = await betObject.save();
+            const result = await (await betObject.save()).populate("user");
             console.log(`Bet was inserted with the _id: ${result._id}`);
+
+            let totalBets = result.user.totalBets + 1;
+
+            const userUpdated = await User
+                .findByIdAndUpdate(
+                    bet.user, 
+                    { totalBets: totalBets },
+                    { new: true }
+                );
+
+            console.log(`User was update with the _id: ${userUpdated._id}`);
     
             res.send(result);
         } catch (err) {
--- models/user.model.js
+++ models/user.model.js
@@ -6,6 +6,18 @@ const UserSchema = new Schema({
         unique: true,
         required: true,
     },
+    winRate: {
+        type: Number,
+        default: 0,
+    },
+    wonTotalBets:{
+        type: Number,
+        default: 0,
+    },
+    totalBets: {
+        type: Number,
+        default: 0,
+    },
 },{ timestamps: true });
 
 const User = model("User", UserSchema);
