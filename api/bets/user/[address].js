1a172efbd0d28ba03d64579bea1bb3c6959d9203
Ian Mungai <iansam74@gmail.com>
2022-08-20 11:24:45 +0300

    fix: get bets via address

--- api/bets/user/[address].js
+++ api/bets/user/[address].js
@@ -18,16 +18,16 @@ module.exports = async (req, res) => {
   
     // This is not a secure way to lookup users. For demo purposes, we did not implement API authorization. 
     // In your application, you should implement some authorization strategy, such as JWTs, that you can validate 
-    // and securely retrieve the user's ID.
-    const { user } = req.query;
+    // and securely retrieve the user's details.
+    const { address } = req.query;
 
-    if (!user) { 
-      res.status(400).send('Bad request: user required');
+    if (!address) { 
+      res.status(400).send('Bad request: address required');
       return;
     }
     
     const bets = await Bet
-      .find({ user }) // Casts a filter based on the user id and returns a list of bets
+      .find({ address }) // Casts a filter based on the user id and returns a list of bets
       .sort('-createdAt') // Sort the bets by createdAt field descending
       .populate('prediction'); // Populate prediction data to each bet
 
