e7f3961c20dae4b7a6b695f3c70cab68278cd371
Ian Mungai <ian.mungai@thisdot.co>
2022-09-12 23:44:36 +0300

    fix: new user error

--- src/contexts/UserDataProvider/index.js
+++ src/contexts/UserDataProvider/index.js
@@ -41,7 +41,27 @@ const UserDataProvider = (props) => {
         address: publicKey.toBase58(),
       }
 
-      return await axiosInstance.post("/api/users/add", newUser);
+      return await axiosInstance.post("/api/users/add", newUser)
+      .then(res => res.data)
+      .then(res => {
+        toast({
+            title: 'New user add',
+            description: `User ID: ${res._id}`,
+            status: 'info',
+            duration: 9000,
+            isClosable: true,
+        })
+        return res;
+      })
+      .catch(err => {
+        toast({
+            title: 'Error creating new user details',
+            description: err.message,
+            status: 'error',
+            duration: 9000,
+            isClosable: true,
+        })
+      });
     }
 
     // Get user from database
@@ -94,28 +114,22 @@ const UserDataProvider = (props) => {
 
     useEffect(() => {
       if (connected) {
-        getUser(publicKey.toBase58());
+        if(!user){
+          getUser(publicKey.toBase58());
+        }else{
+          getMyBets(user);
+          getBalance();
+
+          window.getMyBetsInterval = setInterval(
+            () => getMyBets(user),
+            1000 * 60 * 10 //  1000 ms/s * 60 s/min * 10 min = 10 minutes
+          )
+          return () => {
+            clearInterval(window.getMyBetsInterval)
+          }
+        }
       } else{
         setUser(null);
-        setBalance(null);
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [publicKey]);
-
-    useEffect(() => {
-      if (connected) {
-        getMyBets(user);
-        getBalance();
-
-        window.getMyBetsInterval = setInterval(
-          () => getMyBets(user),
-          1000 * 60 * 10 //  1000 ms/s * 60 s/min * 10 min = 10 minutes
-        )
-        return () => {
-            clearInterval(window.getMyBetsInterval)
-        }
-
-      } else{
         setBalance(null);
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps
