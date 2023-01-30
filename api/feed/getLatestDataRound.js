4be7477ac4cecb0365b081ba7a826123f3cdedb3
Ian Sam Mungai <ian.mungai@thisdot.co>
2023-01-30 18:08:16 +0300

    fix: spelling mistakes README (#173)
    chore: swich from unit8array -> bs58 secretkey
    
    Chore: Ensure base58 private key is supported and update readme
    
    Co-authored-by: Simone Cuomo <simone@thisdot.co>

--- api/feed/getLatestDataRound.js
+++ api/feed/getLatestDataRound.js
@@ -5,81 +5,92 @@ const { Wallet } = require("../../models/wallet.model");
 const { CURRENCY_PAIRS } = require("../../src/lib/constants");
 const { connectToDatabase } = require("../../lib/mongoose");
 const Feed = require("../../models/feed.model");
+const { bs58 } = require("@project-serum/anchor/dist/cjs/utils/bytes");
 
-// creation of wallett using your private key
-const secret = Uint8Array.from(process.env.WALLET_PRIVATE_KEY.split(','));
-const wallet = new Wallet(solanaWeb3.Keypair.fromSecretKey(secret));
+let secretKey;
 
+if (process.env.WALLET_PRIVATE_KEY.includes(",")) {
+  // creation of wallet using your private key
+  secretKey = Uint8Array.from(process.env.WALLET_PRIVATE_KEY.split(","));
+} else {
+  // get private from base58
+  secretKey = bs58.decode(process.env.WALLET_PRIVATE_KEY);
+}
+
+// Escrow Account Wallet
+const wallet = new Wallet(solanaWeb3.Keypair.fromSecretKey(secretKey));
+
+console.log(wallet.publicKey);
 /**
  * This function retrieves the latest price feed data round from Chainlink Data Feeds.
- * 
- * 
+ *
+ *
  * It creates it connects to solana cluster (devnet | mainnet)
  * Then creates an anchor client provider that uses:
  * 1. A solana connection
  * 2. A wallet to sign transactions and pay for fees
  * 3. Options to confirm transactions
- * 
+ *
  * Then retrieves the latest price feed data round from Chainlink Data Feeds.
- * 
+ *
  * For more info view How to get Data Feeds On-Chain (Solana) via the link:
  * https://docs.chain.link/solana/overview
  * @param address Address of the token pair to retrieve the latest data round from
  * @param pair Pair of the token price feed to retrieve the latest data round from
  */
 const getLatestDataRound = async (address, pair) => {
+  let round = null;
 
-    let round = null;
+  //  connection to solana cluster node
+  const connection = new solanaWeb3.Connection(
+    solanaWeb3.clusterApiUrl(process.env.REACT_APP_SOLANA_CLUSTER_NETWORK),
+    "confirmed"
+  );
 
-    //  connection to solana cluster node
-    const connection = new solanaWeb3.Connection(
-        solanaWeb3.clusterApiUrl(process.env.REACT_APP_SOLANA_CLUSTER_NETWORK), 
-        'confirmed'
-    );
+  // creation of a new anchor client provider that uses the connection to solana cluster node
+  const options = anchor.AnchorProvider.defaultOptions(); // default Options for confirming transactions
+  const provider = new anchor.AnchorProvider(connection, wallet, options);
+  anchor.setProvider(provider);
 
-    // creation of a new anchor client provider that uses the connection to solana cluster node
-    const options = anchor.AnchorProvider.defaultOptions(); // default Options for confirming transactions
-    const provider = new anchor.AnchorProvider(connection, wallet, options);
-    anchor.setProvider(provider);
+  const CHAINLINK_FEED_ADDRESS = address;
+  console.log("Fetching pair:", pair);
+  console.log("Fetching address:", address);
 
-    const CHAINLINK_FEED_ADDRESS = address; 
-    console.log('Fetching pair:', pair);
-    console.log('Fetching address:', address);
+  const feedAddress = new anchor.web3.PublicKey(CHAINLINK_FEED_ADDRESS);
 
-    const feedAddress = new anchor.web3.PublicKey(CHAINLINK_FEED_ADDRESS);
+  // load the data feed account using the predefined chainlink program ID
+  const CHAINLINK_PROGRAM_ID = new anchor.web3.PublicKey(
+    process.env.CHAINLINK_PROGRAM_ID_ADDRESS
+  );
+  let dataFeed = await chainlink.OCR2Feed.load(CHAINLINK_PROGRAM_ID, provider);
+  let listener = null;
 
-    // load the data feed account using the predefined chainlink program ID
-    const CHAINLINK_PROGRAM_ID = new anchor.web3.PublicKey(process.env.CHAINLINK_PROGRAM_ID_ADDRESS);
-    let dataFeed = await chainlink.OCR2Feed.load(CHAINLINK_PROGRAM_ID, provider);
-    let listener = null;
-
-    return new Promise(async (res, rej) => {
-        // listen for events from the price feed, and grab the latest rounds' price data
-        listener = dataFeed.onRound(feedAddress, (event) => {
-            round = {
-                pair: pair,
-                feed: address,
-                answer: event.answer,
-                answerToNumber: event.answer.toNumber(),
-                roundId: event.roundId,
-                observationsTS: event.observationsTS,
-                slot: event.slot,
-            };
-            // return the latest round only if event data is available
-            if((round) !== undefined) {
-                provider.connection.removeOnLogsListener(listener);
-                res(round);
-            }
-        });
+  return new Promise(async (res, rej) => {
+    // listen for events from the price feed, and grab the latest rounds' price data
+    listener = dataFeed.onRound(feedAddress, (event) => {
+      round = {
+        pair: pair,
+        feed: address,
+        answer: event.answer,
+        answerToNumber: event.answer.toNumber(),
+        roundId: event.roundId,
+        observationsTS: event.observationsTS,
+        slot: event.slot,
+      };
+      // return the latest round only if event data is available
+      if (round !== undefined) {
+        provider.connection.removeOnLogsListener(listener);
+        res(round);
+      }
     });
-
-}
+  });
+};
 
 /**
- * This function is deployed as a standalone endpoint via Vercel Cloud Functions. 
+ * This function is deployed as a standalone endpoint via Vercel Cloud Functions.
  * Given the expected request query payload, it retrieves the latest price feed data round from Chainlink Data Feeds.
- * The request is expected to come in as a GET request to `/api/feed/getLatestDataRound`. 
- * The request body should have the shape: 
+ * The request is expected to come in as a GET request to `/api/feed/getLatestDataRound`.
+ * The request body should have the shape:
  * { address: "0x...", pair: "XXX-USD" }
  * For more info view How to get Data Feeds On-Chain (Solana) via the link:
  * https://docs.chain.link/solana/overview
@@ -87,55 +98,52 @@ const getLatestDataRound = async (address, pair) => {
  * @param res NextApiResponse HTTP response object wrapped by Vercel function helpers
  */
 module.exports = async (req, res) => {
+  const { cached } = req.query;
 
-    const { cached } = req.query;
+  const updateRoundCache = async (round) => {
+    await Feed.findOneAndUpdate({ feed: round.feed }, round, { upsert: true });
+  };
 
-    const updateRoundCache = async (round) => {
-        await Feed.findOneAndUpdate({ feed: round.feed }, round, {upsert: true})
+  const getRoundsCache = async () => {
+    const feed = await Feed.find();
+
+    if (feed.length === 0) {
+      getLatestDataRounds();
+      return;
     }
 
-    const getRoundsCache = async () => {
-        const feed = await Feed.find();
+    res.status(200).send(feed);
+  };
 
-        if(feed.length === 0 ){
-            getLatestDataRounds(); 
-            return;
-        }
+  const getLatestDataRounds = async () => {
+    const promises = await CURRENCY_PAIRS.map((pair) => {
+      return new Promise((resolve, reject) => {
+        return getLatestDataRound(pair.feedAddress, pair.pair).then(
+          async (res) => {
+            await updateRoundCache(res);
+            resolve(res);
+          }
+        );
+      });
+    });
 
-        res.status(200).send(feed);
-    }
-
-    const getLatestDataRounds = async () => {
-        const promises = await CURRENCY_PAIRS.map(pair => {
-            return new Promise((resolve, reject) => {
-                return getLatestDataRound(pair.feedAddress, pair.pair)
-                .then(async (res) => {
-                    await updateRoundCache(res);
-                    resolve(res);
-                });
-            });
-        });
-
-        Promise.allSettled(promises)
-        .then(response => {
-            res.status(200).send(response);
-        })
-        .catch(err => {
-            res.status(500).send(err);
-        });
-    }
-
-    try {
-        await connectToDatabase();
-        if(cached === 'true'){
-            getRoundsCache();
-        }else{
-            getLatestDataRounds();
-        }
-        
-    }
-
-    catch(err) {
+    Promise.allSettled(promises)
+      .then((response) => {
+        res.status(200).send(response);
+      })
+      .catch((err) => {
         res.status(500).send(err);
+      });
+  };
+
+  try {
+    await connectToDatabase();
+    if (cached === "true") {
+      getRoundsCache();
+    } else {
+      getLatestDataRounds();
     }
-} 
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
--- api/predictions/schedulePredictions.js
+++ api/predictions/schedulePredictions.js
@@ -8,72 +8,85 @@ const User = require("../../models/user.model");
 const { Wallet } = require("../../models/wallet.model");
 const { addMinutes } = require("date-fns");
 const { CURRENCY_PAIRS } = require("../../src/lib/constants");
+const { bs58 } = require("@project-serum/anchor/dist/cjs/utils/bytes");
 
-// Create a wallet for the prediction owner
-const secret = Uint8Array.from(process.env.WALLET_PRIVATE_KEY.split(','));
-const wallet = new Wallet(solanaWeb3.Keypair.fromSecretKey(secret));
+let secretKey;
+
+if (process.env.WALLET_PRIVATE_KEY.includes(",")) {
+  // creation of wallet using your private key
+  secretKey = Uint8Array.from(process.env.WALLET_PRIVATE_KEY.split(","));
+} else {
+  // get private from base58
+  secretKey = bs58.decode(process.env.WALLET_PRIVATE_KEY);
+}
+
+// Escrow Account Wallet
+const wallet = new Wallet(solanaWeb3.Keypair.fromSecretKey(secretKey));
 
 /**
  * This function retrieves the latest price feed data round from Chainlink Data Feeds.
- * 
+ *
  * It creates it connects to solana cluster (devnet | mainnet)
  * Then creates an anchor client provider that uses:
  * 1. A solana connection
  * 2. A wallet to sign transactions and pay for fees
  * 3. Options to confirm transactions
- * 
+ *
  * Then retrieves the latest price feed data round from Chainlink Data Feeds.
- * 
+ *
  * This function is used in conjuction with github actions to update the schedule hourly predictions using cron.
  * Checkout .github/workflows/predictions-cron.yml for more details
  * This function can be used AWS SQS or Lambda as well
- * 
+ *
  * For more info view How to get Data Feeds On-chain (Solana) via the link:
  * https://docs.chain.link/solana/overview
  * @param address Address of the token pair to retrieve the latest data round from
  * @param pair Pair of the token price feed to retrieve the latest data round from
  */
 const getLatestDataRound = async (address, pair) => {
+  let round = null;
 
-    let round = null;
+  //  connection to solana cluster node
+  const connection = new solanaWeb3.Connection(
+    solanaWeb3.clusterApiUrl(process.env.REACT_APP_SOLANA_CLUSTER_NETWORK),
+    "confirmed"
+  );
 
-    //  connection to solana cluster node
-    const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl(process.env.REACT_APP_SOLANA_CLUSTER_NETWORK), 'confirmed');
+  // creation of a new anchor client provider without use of node server & id.json
+  const options = anchor.AnchorProvider.defaultOptions();
+  const provider = new anchor.AnchorProvider(connection, wallet, options);
+  anchor.setProvider(provider);
 
-    // creation of a new anchor client provider without use of node server & id.json
-    const options = anchor.AnchorProvider.defaultOptions();
-    const provider = new anchor.AnchorProvider(connection, wallet, options);
-    anchor.setProvider(provider);
+  const CHAINLINK_FEED_ADDRESS = address;
+  const feedAddress = new anchor.web3.PublicKey(CHAINLINK_FEED_ADDRESS);
 
-    const CHAINLINK_FEED_ADDRESS = address; 
-    const feedAddress = new anchor.web3.PublicKey(CHAINLINK_FEED_ADDRESS);
+  // load the data feed account using the predefined chainlink program ID
+  const CHAINLINK_PROGRAM_ID = new anchor.web3.PublicKey(
+    "cjg3oHmg9uuPsP8D6g29NWvhySJkdYdAo9D25PRbKXJ"
+  );
+  let dataFeed = await chainlink.OCR2Feed.load(CHAINLINK_PROGRAM_ID, provider);
+  let listener = null;
 
-    // load the data feed account using the predefined chainlink program ID
-    const CHAINLINK_PROGRAM_ID = new anchor.web3.PublicKey("cjg3oHmg9uuPsP8D6g29NWvhySJkdYdAo9D25PRbKXJ");
-    let dataFeed = await chainlink.OCR2Feed.load(CHAINLINK_PROGRAM_ID, provider);
-    let listener = null;
-
-    return new Promise(async (res, rej) => {
-        // listen for events from the price feed, and grab the latest rounds' price data
-        listener = dataFeed.onRound(feedAddress, (event) => {
-            round = {
-                pair: pair,
-                feed: address,
-                answer: event.answer,
-                answerToNumber: event.answer.toNumber(),
-                roundId: event.roundId,
-                observationsTS: event.observationsTS,
-                slot: event.slot,
-            };
-            // return the latest round only if event data is available
-            if((round) !== undefined) {
-                provider.connection.removeOnLogsListener(listener);
-                res(round);
-            }
-        });
+  return new Promise(async (res, rej) => {
+    // listen for events from the price feed, and grab the latest rounds' price data
+    listener = dataFeed.onRound(feedAddress, (event) => {
+      round = {
+        pair: pair,
+        feed: address,
+        answer: event.answer,
+        answerToNumber: event.answer.toNumber(),
+        roundId: event.roundId,
+        observationsTS: event.observationsTS,
+        slot: event.slot,
+      };
+      // return the latest round only if event data is available
+      if (round !== undefined) {
+        provider.connection.removeOnLogsListener(listener);
+        res(round);
+      }
     });
-
-}
+  });
+};
 
 /**
  * Given the expected prediction entity, this function generates a new prediction entity and stores it in MongoDB.
@@ -81,32 +94,31 @@ const getLatestDataRound = async (address, pair) => {
  * @param prediction Prediction entity (see api/models/prediction.model.js).
  */
 const createPrediction = async (prediction) => {
-    try {
-        await connectToDatabase();
-        
-        const predictionObject = new Prediction(prediction);
-        
-        const result = await predictionObject.save();
-        console.log(`Prediction was inserted with the _id: ${result._id}`);
+  try {
+    await connectToDatabase();
 
-        return result;
+    const predictionObject = new Prediction(prediction);
 
-    } catch (err) {
-        console.error("Failed to create new prediction, with error code: " + err.message);
-    } 
-  
-}
+    const result = await predictionObject.save();
+    console.log(`Prediction was inserted with the _id: ${result._id}`);
 
-/** 
- * 
+    return result;
+  } catch (err) {
+    console.error(
+      "Failed to create new prediction, with error code: " + err.message
+    );
+  }
+};
+
+/**
+ *
  * @param date Date object
  * @param minutes number of minutes to add to the date
  * @returns date with added minutes
-*/
+ */
 const addMinutesToDate = (date, minutes) => {
-    return addMinutes(date, minutes);//  1000 ms/s * 60 s/min * min = # ms
-}
-
+  return addMinutes(date, minutes); //  1000 ms/s * 60 s/min * min = # ms
+};
 
 /**
  * This function creates 2 predictions for each pair in the pairs array.
@@ -114,154 +126,144 @@ const addMinutesToDate = (date, minutes) => {
  * The expiry time is set to the next hour.
  * The deadline is set to the 10 minutes before the expiry time.
  *
-*/
+ */
 const schedulePredictions = () => {
-    
-    const promises = CURRENCY_PAIRS.map(async (pair) => {
+  const promises = CURRENCY_PAIRS.map(async (pair) => {
+    const { feedAddress, pair: currency } = pair;
 
-        const { feedAddress, pair: currency} = pair;
+    const latestRound = await getLatestDataRound(feedAddress, currency);
 
-        const latestRound = await getLatestDataRound(feedAddress, currency);
-    
-        const { answerToNumber, observationsTS, slot, roundId } = latestRound;
-    
-        const date = new Date();
+    const { answerToNumber, observationsTS, slot, roundId } = latestRound;
 
-        const predictionData = {
-            owner: wallet.publicKey,
-            account: feedAddress,
-            pair: currency,
-            expiryTime: addMinutesToDate(date, 120),
-            predictionDeadline: addMinutesToDate(date, 60),
-            openingPredictionPrice: answerToNumber,
-            openingPredictionTime: observationsTS,
-            openingPredictionSlot: slot,
-            openingPredictionRoundId: roundId,
-            ROI: 2,
-        };
-    
-        const plusOnePercent = await createPrediction({
-            ...predictionData,
-            predictionPrice: answerToNumber * 1.01,
-            direction: true,
-        });
-        const minusOnePercent = await createPrediction({
-            ...predictionData,
-            predictionPrice: answerToNumber * 0.99,
-            direction: false,
-        });
-    
-        return [plusOnePercent, minusOnePercent];
+    const date = new Date();
+
+    const predictionData = {
+      owner: wallet.publicKey,
+      account: feedAddress,
+      pair: currency,
+      expiryTime: addMinutesToDate(date, 120),
+      predictionDeadline: addMinutesToDate(date, 60),
+      openingPredictionPrice: answerToNumber,
+      openingPredictionTime: observationsTS,
+      openingPredictionSlot: slot,
+      openingPredictionRoundId: roundId,
+      ROI: 2,
+    };
+
+    const plusOnePercent = await createPrediction({
+      ...predictionData,
+      predictionPrice: answerToNumber * 1.01,
+      direction: true,
+    });
+    const minusOnePercent = await createPrediction({
+      ...predictionData,
+      predictionPrice: answerToNumber * 0.99,
+      direction: false,
     });
 
-    return Promise.all(promises)
-}
+    return [plusOnePercent, minusOnePercent];
+  });
 
+  return Promise.all(promises);
+};
 
 /**
- * It updates all bet entities from MongoDB with a 'status' of 'ongoing' via the Mongoose driver. 
+ * It updates all bet entities from MongoDB with a 'status' of 'ongoing' via the Mongoose driver.
  * Checks if the bet expiryTime has passed
  * if so, updates the bet status based on the direction of the prediction and opening prediction price to 'won' or 'lost'.
- * 
+ *
  */
 const updateBetStatus = async () => {
-    const bets = await Bet
-        .find({ status: "ongoing" }) // casts a filter based on the query object and returns a list of bets with status 'ongoing'
-        .populate("prediction"); // Populate prediction data to each bet
+  const bets = await Bet.find({ status: "ongoing" }) // casts a filter based on the query object and returns a list of bets with status 'ongoing'
+    .populate("prediction"); // Populate prediction data to each bet
 
+  const promises = bets.map(async (bet) => {
+    const { prediction } = bet;
+    const { pair, account, direction, openingPredictionPrice, expiryTime } =
+      prediction;
 
-        const promises = bets.map(async (bet) => {
-            const { prediction } = bet;
-            const { pair, account, direction, openingPredictionPrice, expiryTime } =  prediction;
-            
-            const { answerToNumber } = await getLatestDataRound(account, pair);
-            
-            // Check if the bet expiryTime has passed if not the function returns
-            if(expiryTime > new Date().toISOString()) {
-                return bet;
-            }
+    const { answerToNumber } = await getLatestDataRound(account, pair);
 
-            let currentStatus = 'ongoing';
+    // Check if the bet expiryTime has passed if not the function returns
+    if (expiryTime > new Date().toISOString()) {
+      return bet;
+    }
 
-            if(direction){
-                openingPredictionPrice < answerToNumber
-                ? currentStatus = 'won' 
-                : currentStatus = 'lost';
-            }else{
-                openingPredictionPrice > answerToNumber
-                ? currentStatus = 'won' 
-                : currentStatus = 'lost';
-            }
+    let currentStatus = "ongoing";
 
-        const result = await Bet
-            .findOneAndUpdate({ _id: bet._id }, { status: currentStatus }, { new: true })
-            .populate("user");
-        console.log(`Bet was inserted with the _id: ${result._id}`);
+    if (direction) {
+      openingPredictionPrice < answerToNumber
+        ? (currentStatus = "won")
+        : (currentStatus = "lost");
+    } else {
+      openingPredictionPrice > answerToNumber
+        ? (currentStatus = "won")
+        : (currentStatus = "lost");
+    }
 
-        const user = result.user;
-        const {
-            wonTotalBets,
-            winRate,
-            totalBets,
-        } = user;
+    const result = await Bet.findOneAndUpdate(
+      { _id: bet._id },
+      { status: currentStatus },
+      { new: true }
+    ).populate("user");
+    console.log(`Bet was inserted with the _id: ${result._id}`);
 
-        let totalWonBets = wonTotalBets; 
-        let win_rate = winRate;
+    const user = result.user;
+    const { wonTotalBets, winRate, totalBets } = user;
 
-        if(currentStatus === 'won') {
-            totalWonBets += 1;
-        }
+    let totalWonBets = wonTotalBets;
+    let win_rate = winRate;
 
-        win_rate = (totalWonBets / totalBets) * 100;
+    if (currentStatus === "won") {
+      totalWonBets += 1;
+    }
 
-        const userUpdated = await User
-        .findByIdAndUpdate(
-            user._id, 
-            { winRate: win_rate, wonTotalBets: totalWonBets }, 
-            { new: true }
-        );
-        console.log(`User was updated with the _id: ${userUpdated._id}`);
+    win_rate = (totalWonBets / totalBets) * 100;
 
-        return result;
-    })
-    
-    return Promise.all(promises)
-}
+    const userUpdated = await User.findByIdAndUpdate(
+      user._id,
+      { winRate: win_rate, wonTotalBets: totalWonBets },
+      { new: true }
+    );
+    console.log(`User was updated with the _id: ${userUpdated._id}`);
+
+    return result;
+  });
+
+  return Promise.all(promises);
+};
 
 /**
- * This function is deployed as a standalone endpoint via Vercel Cloud Functions. 
- * The request is expected to come in as a POST request to `/api/predictions/schedulePredictions`. 
- * 
- * This function is used in conjuction with github actions to schedule predictions and update the status 
+ * This function is deployed as a standalone endpoint via Vercel Cloud Functions.
+ * The request is expected to come in as a POST request to `/api/predictions/schedulePredictions`.
+ *
+ * This function is used in conjuction with github actions to schedule predictions and update the status
  * of all bets on a hourly basis using cron.
  * Checkout .github/workflows/bet-status-cron.yml for more details
  * This function can be used AWS SQS or Lambda as well
- * 
+ *
  * @param req NextApiRequest HTTP request object wrapped by Vercel function helpers
  * @param res NextApiResponse HTTP response object wrapped by Vercel function helpers
-*/
+ */
 module.exports = async (req, res) => {
+  if (req.method === "POST") {
+    const { authorization } = req.headers;
 
-    if (req.method === 'POST') {
-        const { authorization } = req.headers;
-
-        if (authorization !== `Bearer ${process.env.API_SECRET_KEY}`) {
-            res.status(401).send('Unauthorized');
-            return;
-        }
-
-        try{
-            res.status(200).send({
-                predictions: await schedulePredictions(),
-                betStatus: await updateBetStatus()
-            });
-        }
-        catch(err) {
-            res.status(500).send(err);
-        }
-    
-    } else {
-        res.setHeader('Allow', 'POST');
-        res.status(405).end('Method Not Allowed');
+    if (authorization !== `Bearer ${process.env.API_SECRET_KEY}`) {
+      res.status(401).send("Unauthorized");
+      return;
     }
+
+    try {
+      res.status(200).send({
+        predictions: await schedulePredictions(),
+        betStatus: await updateBetStatus(),
+      });
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
+};
